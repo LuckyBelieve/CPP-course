bd92636c007b698489553de4d29067755bf2169c
LuckyBelieve <ineza@heybuddy.co.in>
2024-01-18 11:50:49 +0200

    Linked lists with structures

---
+++ intermediate/LinkedLists/Linked.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    Node *next;
+
+    Node(int d){
+        data = d;
+        next = NULL;
+    }
+};
+
+void PrintList(Node *node){
+    while (node != NULL)
+    {
+        cout << node->data << endl;
+        node = node->next;
+    }
+}
+
+int main(){
+    Node *node1 = new Node(12);
+    Node *node2 = new Node(14);
+    Node *node3 = new Node(16);
+    Node *node4 = new Node(18);
+
+    node1->next = node2;
+    node2->next = node3;
+    node3->next = node4;
+
+    Node *head = node1;
+    PrintList(head);
+
+    return 0;
+};
